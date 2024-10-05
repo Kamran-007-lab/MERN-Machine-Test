c81adeb4e4d2de32455ce001441baa81a21b3081
Kamran <kamran.khan.03112002@gmail.com>
2024-10-05 16:29:08 +0530

    Added multer for image uploads

---
+++ backend/src/app.js
@@ -0,0 +1,39 @@
+import express from "express"
+import cors from "cors"
+import cookieParser from "cookie-parser"
+import bodyParser from "body-parser"
+// var bodyParser = require('body-parser');
+
+const app = express()
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}))
+
+//Middleware Configuration for accepting data via JSON or URL
+app.use(express.json({limit: "16kb"}))
+app.use(express.urlencoded({extended: true,limit: "16kb"}))
+app.use(express.static("public"))
+app.use(cookieParser())
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+//routes import
+// import userRouter from "./routes/user.routes.js"
+// import videoRouter from "./routes/video.routes.js"
+// import commentRouter from "./routes/comment.routes.js"
+// import likeRouter from "./routes/like.routes.js"
+// import subscriptionRouter from "./routes/subscription.routes.js"
+
+//routes declaraton
+// app.use("/api/v1/users",userRouter)
+// app.use("/api/v1/videos",videoRouter)
+// app.use("/api/v1/comments",commentRouter)
+// app.use("/api/v1/likes",likeRouter)
+// app.use("/api/v1/subscriptions",subscriptionRouter)
+
+
+
+export default app
